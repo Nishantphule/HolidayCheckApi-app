def0d92ff65bcfc04555133275345af0e3a734eb
Nishantphule <nishantphule12@gmail.com>
2023-04-12 15:21:06 +0530

    form validation and trycatch

--- holiday-api.js
+++ holiday-api.js
@@ -1,9 +1,27 @@
 
 const holidayArea = document.getElementById("holidays-area")
+const formObj = document.getElementById('holidaysForm');
+
+function formValidation(data) {
+    const country = data.countryCode.split("").length
+    const month = data.month
+    if (country !== 2) {
+        document.getElementById("countryError").style.display = "block"
+    }
+    else if (month < 0 || month > 12) {
+        document.getElementById("monthError").style.display = "block"
+    }
+    else {
+        document.getElementById("countryError").style.display = "none"
+        document.getElementById("monthError").style.display = "none"
+        getHolidays(data.countryCode, data.month);
+    }
+}
+
 
 
 // getting form details on submit
-const formObj = document.getElementById('holidaysForm');
+
 formObj.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -13,8 +31,12 @@ formObj.addEventListener('submit', (e) => {
             data[element.name] = element.value;
         }
     });
-    getHolidays(data.countryCode, data.month);
+    formValidation(data)
+
 })
+
+
+
 
 
 // fetching monthly holidays using form data
@@ -28,46 +50,56 @@ const getHolidays = async (code, month) => {
     loading.innerHTML = '<div class="spinner-border"></div>'
     holidayArea.appendChild(loading)
 
-    // fetching holiday api
-    const response = await fetch(`https://holidays.abstractapi.com/v1/?api_key=0cff6f896b4747eaacce2349b2167c14&country=${code}&year=2020&month=${month}`)
 
-    // removeing element for loading text
-    holidayArea.removeChild(loading)
+    try {
+        // fetching holiday api
+        const response = await fetch(`https://holidays.abstractapi.com/v1/?api_key=0cff6f896b4747eaacce2349b2167c14&country=${code}&year=2020&month=${month}`)
+
+        // removeing element for loading text
+        holidayArea.removeChild(loading)
 
 
-    // creating title
-    const title = document.createElement("h1")
-    title.id = 'title'
-    title.style.width = "100%"
-    title.style.backgroundColor = "#D8C1DE"
-    const monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    let monthName = monthList[month - 1]
-    title.innerText = `Holidays in ${monthName} month`
-    holidayArea.appendChild(title)
+        // creating title
+        const title = document.createElement("h1")
+        title.id = 'title'
+        title.style.width = "100%"
+        title.style.backgroundColor = "#D8C1DE"
+        const monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        let monthName = monthList[month - 1]
 
-    // iterating through each holiday to create a card
-    const holidays = await response.json();
-    holidays.forEach((holiday) => {
 
-        // card for each holiday
-        const holidayDetails = document.createElement("div")
-        holidayDetails.classList += "holidayCard"
+        // iterating through each holiday to create a card
+        const holidays = await response.json();
+        const location = await holidays[0].location
 
-        // name element
-        const holidayName = document.createElement("h2")
-        holidayName.style.color = "black"
-        holidayName.innerText = `${holiday.name}`
+        title.innerText = `Holidays of ${monthName} month in ${location}`
+        holidayArea.appendChild(title)
 
-        // day and date element
-        const holidayDay = document.createElement("h3")
-        holidayDay.innerText = `${holiday.date} ${holiday.week_day}`
+        holidays.forEach((holiday) => {
 
-        // type of holiday element
-        const holidayType = document.createElement("h3")
-        holidayType.innerText = `Holiday Type-${holiday.type}`
+            // card for each holiday
+            const holidayDetails = document.createElement("div")
+            holidayDetails.classList += "holidayCard"
 
-        holidayDetails.append(holidayName, holidayDay, holidayType)
-        holidayArea.append(holidayDetails)
-    })
+            // name element
+            const holidayName = document.createElement("h2")
+            holidayName.style.color = "black"
+            holidayName.innerText = `${holiday.name}`
+
+            // day and date element
+            const holidayDay = document.createElement("h3")
+            holidayDay.innerText = `${holiday.date} ${holiday.week_day}`
+
+            // type of holiday element
+            const holidayType = document.createElement("h3")
+            holidayType.innerText = `Holiday Type-${holiday.type}`
+
+            holidayDetails.append(holidayName, holidayDay, holidayType)
+            holidayArea.append(holidayDetails)
+        })
+    } catch (error) {
+        alert("Invalid Input")
+        console.error(error)
+    }
 
 };
